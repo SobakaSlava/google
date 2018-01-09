5f685b6011e3fcf2d1fe5a4eed6bd53467ee5d84
SobakaSlava <nikita_solo_11@mail.ru>
2018-01-09 16:58:02 +0300

    almost done. have to add some scripts

---
+++ script.js
@@ -0,0 +1,6 @@
+function lucky() {
+  location.href = "https://www.google.com/doodles";
+}
+function signin() {
+  location.href = "https://accounts.google.com/ServiceLogin?hl=ru&passive=true&continue=https://www.google.ru/"
+}
